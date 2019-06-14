f461de2b34cf776b10cdad725cb054ea93f5fb9e
itzAki07 <valdimor@gmail.com>
2019-06-14 13:19:51 +0530

    sf

--- script/script.js
+++ script/script.js
@@ -9,6 +9,13 @@ function scrollFunction() {
         document.getElementById("myBtn").style.display = "none";
     }
 }
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById("myBtns").style.display = "block";
+    } else {
+        document.getElementById("myBtns").style.display = "none";
+    }
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
