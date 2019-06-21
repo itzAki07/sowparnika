930c1be6274cc5f07f3e832a7b20522534e18e6f
itzAki07 <valdimor@gmail.com>
2019-06-21 10:44:59 +0530

    sfsag

--- script/script.js
+++ script/script.js
@@ -1,21 +1,18 @@
 window.onscroll = function () {
     scrollFunction()
-};
+}
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
     }
 }
-function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("myBtns").style.display = "block";
-    } else {
-        document.getElementById("myBtns").style.display = "none";
-    }
-}
+
+// $(document).scroll(function () {
+//     $("#myBtn").toggleClass("navbar-scrolled", $(document).scrollTop() >= 209);
+// });
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
